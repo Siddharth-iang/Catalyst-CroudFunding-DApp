6476758be05cc1faaa70c5a63f5b2046897d0be6
Siddhartha <siddureddyvr1115@email.com>
2025-12-26 00:22:55 +0530

    Updated contract address to Sepolia

--- BlockFund/hardhat.config.js
+++ BlockFund/hardhat.config.js
@@ -1,15 +1,20 @@
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
-  defaultNetwork: 'localhost',
+  defaultNetwork: "localhost",
   networks: {
     localhost: {
-      url: 'http://127.0.0.1:8545',
+      url: "http://127.0.0.1:8545",
     },
+    sepolia: {
+      // I have pasted your URL and Key directly here to bypass the error
+      url: "https://sepolia.infura.io/v3/bbd9d58fe2e44eeea6617628dddc185c",
+      accounts: ["0x1abd7ea8880f266c9dffbab608d59782b5744486704057a817409ca0de45e10a"]
+    }
   },
   solidity: {
-    version: '0.8.11',
+    version: "0.8.11",
     settings: {
       optimizer: {
         enabled: true,
@@ -18,10 +23,10 @@ module.exports = {
     },
   },
   paths: {
-    sources: './src/contracts',
-    artifacts: './src/abis',
+    sources: "./src/contracts",
+    artifacts: "./src/abis",
   },
   mocha: {
     timeout: 40000,
   },
-}
+};
